efa4a6419066f626f548f4027ab3988dae13fa01
thecoocko <butenkonaddezhda@gmail.com>
2023-08-19 16:28:08 +0200

    d

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,9 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
+            if (str.startsWith("0") && str.charAt(1)!='.'){
+                return false;
+            }
             return Double.valueOf(str) > 0 ? true : false;
         }
         catch (Exception e){
